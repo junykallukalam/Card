d35c89dcde7d0c2d58062596e59e074fbb757454
Juny <juny.kallukalam@gmail.com>
2023-06-07 13:25:51 -0400

    Initial Commit

---
+++ card/cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardApp.swift
+//  card
+//
+//  Created by juny kallukalam on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
